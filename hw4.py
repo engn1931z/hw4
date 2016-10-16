1bf2570bb4da517ae33779af2a70fcfe604e292b
rashidzia <Rashid_Zia@brown.edu>
2016-10-16 01:07:06 -0400

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -34,7 +34,7 @@ desiredAngles=[float(x) for x in serverRequest.split(',')]
 ### YOUR CODE TO CREATE THE COMMAND SEQUENCE SHOULD GO BELOW HERE
 ##################################################################
 
-
+import struct
 # import anything else you might need here 
 
 email= "YOUR_EMAIL_GOES_HERE@brown.edu" #REPLACE THIS
