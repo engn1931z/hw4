0b949a2c257a4f9eef7932234654a44acd9b4eed
rashidzia <Rashid_Zia@brown.edu>
2016-10-15 23:55:10 -0400

    submission files added

---
+++ submit.py
@@ -0,0 +1,26 @@
+#Run this script at the end when you are ready to submit your homework to the autograder.
+
+import hw4  # imports your hw2 module
+import requests
+import json
+
+submissionFile=open('hw4.py','r')
+postParams=hw4.yourSubmission()
+del postParams["output"] 
+postData={"output":hw4.yourSubmission()["output"]}
+postHeaders={'content-type': 'application/octet-stream'}
+
+tokenFile=open('token','a+')
+token=tokenFile.read();
+
+if len(token)<6: 
+	tokenResponse=requests.post("https://script.google.com/macros/s/AKfycbyiAaZXZHG0PzuWNHSl5gPXTCa2i15tTmavDSA3wAHz11DOnqbM/exec",data={'requestingToken':1,'email':postParams["email"]});
+	token=tokenResponse.text;
+	tokenFile.write(token)
+
+postParams["token"]=token
+postParams["submission"]=submissionFile.read()
+subResponse=requests.post("https://script.google.com/macros/s/AKfycbyiAaZXZHG0PzuWNHSl5gPXTCa2i15tTmavDSA3wAHz11DOnqbM/exec",params=postParams,data=postData,headers = postHeaders)
+responseFile=open('submissionResponse.txt','w+')
+responseFile.write(subResponse.text.encode('utf8'))
+print subResponse.text
