0e63d7423b85aa5e3fe903d74d327306bc3df60d
Matthew Huang (Shao Ran) <shao_ran_huang@brown.edu>
2018-02-06 13:31:03 -0500

    Switch to python 3

--- submit.py
+++ submit.py
@@ -1,25 +1,26 @@
 #Run this script at the end when you are ready to submit your homework to the autograder.
 
-import hw4  # imports your hw4 module
+import hw_sniffing  # imports your hw_sniffing module
 import requests
 
-submissionFile=open('hw4.py','r')
-postParams=hw4.yourSubmission()
+submissionFile=open('hw_sniffing.py','r')
+postParams=hw_sniffing.yourSubmission()
 del postParams["output"] 
-postData={"output":hw4.yourSubmission()["output"]}
+postData={"output":bytes(hw_sniffing.yourSubmission()["output"],encoding='iso-8859-1')}
 postHeaders={'content-type': 'application/octet-stream'}
 
 tokenFile=open('token','a+')
 token=tokenFile.read();
 
 if len(token)<6: 
-	tokenResponse=requests.post("https://script.google.com/macros/s/AKfycbyiAaZXZHG0PzuWNHSl5gPXTCa2i15tTmavDSA3wAHz11DOnqbM/exec",data={'requestingToken':1,'email':postParams["email"]});
+	tokenResponse=requests.post("https://script.google.com/a/brown.edu/macros/s/AKfycbyiAaZXZHG0PzuWNHSl5gPXTCa2i15tTmavDSA3wAHz11DOnqbM/exec",data={'requestingToken':1,'email':postParams["email"]});
 	token=tokenResponse.text;
 	tokenFile.write(token)
 
 postParams["token"]=token
 postParams["submission"]=submissionFile.read()
-subResponse=requests.post("https://script.google.com/macros/s/AKfycbyiAaZXZHG0PzuWNHSl5gPXTCa2i15tTmavDSA3wAHz11DOnqbM/exec",params=postParams,data=postData,headers = postHeaders)
-responseFile=open('submissionResponse.txt','w+')
-responseFile.write(subResponse.text.encode('utf8'))
-print subResponse.text
+
+subResponse=requests.post("https://script.google.com/a/brown.edu/macros/s/AKfycbyiAaZXZHG0PzuWNHSl5gPXTCa2i15tTmavDSA3wAHz11DOnqbM/exec",params=postParams,data=postData,headers = postHeaders)
+responseFile=open('submissionResponse.txt','wb')
+responseFile.write(subResponse.text.encode('utf-8'))
+print(subResponse.text)
